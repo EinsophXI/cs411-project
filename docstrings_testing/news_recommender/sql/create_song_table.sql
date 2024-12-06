d0c5e79877117c23e9e8b2780d0f29f81b15d8d8
alexcobblin <alexcobblin@gmail.com>
2024-12-06 16:16:49 -0500

    fe

--- docstrings_testing/news_recommender/sql/create_song_table.sql
+++ docstrings_testing/news_recommender/sql/create_song_table.sql
@@ -2,10 +2,10 @@ DROP TABLE IF EXISTS articles;
 CREATE TABLE articles (
     name TEXT NOT NULL,
     author TEXT NOT NULL,
-    title TEXT NOT NULL
-    url TEXT NOT NULL
-    content TEXT NOT NULL
-    publishedAt TEXT NOT NULL
+    title TEXT NOT NULL,
+    url TEXT NOT NULL,
+    content TEXT NOT NULL,
+    publishedAt TEXT NOT NULL,
     deleted BOOLEAN DEFAULT FALSE,
     UNIQUE(author, title, url)
 );
