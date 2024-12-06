e79dd54e70c61e2ccfd0d5066b1469fa9a892576
EinsophXI <hwangr@bu.edu>
2024-12-06 15:43:00 -0500

    moved get article

--- docstrings_testing/playlist/music_collection/utils/random_utils.py
+++ docstrings_testing/playlist/music_collection/utils/random_utils.py
@@ -1,15 +1,13 @@
 import logging
 import requests
 
-# from music_collection.utils.logger import configure_logger
+from music_collection.utils.logger import configure_logger
 
 logger = logging.getLogger(__name__)
-# configure_logger(logger)
+configure_logger(logger)
 
 
-
-
-def get_article(keyword: str) -> str:
+def get_random(num_songs: int) -> int:
     """
     Fetches a random int between 1 and the number of songs in the catalog from random.org.
 
@@ -20,33 +18,31 @@ def get_article(keyword: str) -> str:
         RuntimeError: If the request to random.org fails or returns an invalid response.
         ValueError: If the response from random.org is not a valid float.
     """
-    url = f"https://newsapi.org/v2/everything?q={keyword}&pagesize=1&apiKey=e616acff8a674cfc8ba4648026e85f1d"
+    url = f"https://www.random.org/integers/?num=1&min=1&max={num_songs}&col=1&base=10&format=plain&rnd=new"
 
     try:
         # Log the request to random.org
-        logger.info("Fetching article from %s", url)
+        logger.info("Fetching random number from %s", url)
 
         response = requests.get(url, timeout=5)
 
         # Check if the request was successful
         response.raise_for_status()
 
-        article = response.text.strip()
+        random_number_str = response.text.strip()
 
-        # try:
-        #     random_number = int(random_number_str)
-        # except ValueError:
-        #     raise ValueError("Invalid response from random.org: %s" % random_number_str)
+        try:
+            random_number = int(random_number_str)
+        except ValueError:
+            raise ValueError("Invalid response from random.org: %s" % random_number_str)
 
-        print("Received article: %.3f", article)
-        return article
+        logger.info("Received random number: %.3f", random_number)
+        return random_number
 
     except requests.exceptions.Timeout:
-        logger.error("Request to newsapi.org timed out.")
-        raise RuntimeError("Request to newsapi.org timed out.")
+        logger.error("Request to random.org timed out.")
+        raise RuntimeError("Request to random.org timed out.")
 
     except requests.exceptions.RequestException as e:
-        logger.error("Request to newsapi.org failed: %s", e)
-        raise RuntimeError("Request to newsapi.org failed: %s" % e)
-
-get_article("tesla")
+        logger.error("Request to random.org failed: %s", e)
+        raise RuntimeError("Request to random.org failed: %s" % e)
